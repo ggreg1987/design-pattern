df0049c3f9b7141399b8d82e57858c78ff68bdd3
ggreg1987 <gabriel.gregorio1987@gmail.com>
2023-03-29 18:41:36 -0300

    Handle CpfNotFoundException configuration

--- ms-person/src/main/java/br/com/dio/exception/controller/ApplicationControllerAdvice.java
+++ ms-person/src/main/java/br/com/dio/exception/controller/ApplicationControllerAdvice.java
@@ -2,11 +2,13 @@ package br.com.dio.exception.controller;
 
 import br.com.dio.exception.ApiError;
 import br.com.dio.exception.CpfBadRequestException;
+import br.com.dio.exception.CpfNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestControllerAdvice
 public class ApplicationControllerAdvice {
@@ -14,6 +16,12 @@ public class ApplicationControllerAdvice {
   @ExceptionHandler(CpfBadRequestException.class)
   @ResponseStatus(BAD_REQUEST)
   public ApiError handleCpfException(CpfBadRequestException ex) {
+    return message(ex);
+  }
+
+  @ExceptionHandler(CpfNotFoundException.class)
+  @ResponseStatus(NOT_FOUND)
+  public ApiError handleCpfNotFoundException(CpfNotFoundException ex) {
     return message(ex);
   }
 
