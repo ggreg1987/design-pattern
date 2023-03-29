834267c85213c4d1920bcf2d55618e1a536ed4b2
ggreg1987 <gabriel.gregorio1987@gmail.com>
2023-03-29 20:12:22 -0300

    Creating a sendMessage method,where will send a PersonDTO

--- ms-person/src/main/java/br/com/dio/rabbitMessage/SendMessage.java
+++ ms-person/src/main/java/br/com/dio/rabbitMessage/SendMessage.java
@@ -1,4 +1,26 @@
 package br.com.dio.rabbitMessage;
 
+import br.com.dio.domain.rest.dto.PersonDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class SendMessage {
+
+  @Value("${rabbitmq.person.exchange}")
+  private String exchange;
+  @Value("${rabbitmq.person.routing-key}")
+  private String routingKey;
+
+  private final RabbitTemplate template;
+
+  public void sendMessage(PersonDTO dto) {
+    System.out.println(dto);
+    System.out.println(exchange);
+    System.out.println(routingKey);
+    template.convertAndSend(exchange,routingKey,dto);
+  }
 }
