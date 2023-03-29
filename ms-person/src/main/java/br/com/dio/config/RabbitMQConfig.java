e00e4da3767290291adf5b0b58228f292b815a7c
ggreg1987 <gabriel.gregorio1987@gmail.com>
2023-03-29 20:08:36 -0300

    Creating a RabbitTemplate

--- ms-person/src/main/java/br/com/dio/config/RabbitMQConfig.java
+++ ms-person/src/main/java/br/com/dio/config/RabbitMQConfig.java
@@ -2,8 +2,11 @@ package br.com.dio.config;
 
 import org.springframework.amqp.core.Exchange;
 import org.springframework.amqp.core.ExchangeBuilder;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,6 +23,14 @@ public class RabbitMQConfig {
         .directExchange(exchange)
         .durable(true)
         .build();
+  }
+
+  @Bean
+  @Autowired
+  public RabbitTemplate getTemplate(ConnectionFactory factory) {
+    final var rbTemplate = new RabbitTemplate(factory);
+    rbTemplate.setMessageConverter(convertMessage());
+    return rbTemplate;
   }
 
 
