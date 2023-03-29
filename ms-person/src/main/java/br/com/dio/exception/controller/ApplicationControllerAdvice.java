f77ce4dd6b51e88ef77eec3793f5478f0e1a82b5
ggreg1987 <gabriel.gregorio1987@gmail.com>
2023-03-29 18:38:45 -0300

    Handle CpfBadRequestException configuration

--- ms-person/src/main/java/br/com/dio/exception/controller/ApplicationControllerAdvice.java
+++ ms-person/src/main/java/br/com/dio/exception/controller/ApplicationControllerAdvice.java
@@ -1,12 +1,21 @@
 package br.com.dio.exception.controller;
 
 import br.com.dio.exception.ApiError;
+import br.com.dio.exception.CpfBadRequestException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @RestControllerAdvice
 public class ApplicationControllerAdvice {
 
-
+  @ExceptionHandler(CpfBadRequestException.class)
+  @ResponseStatus(BAD_REQUEST)
+  public ApiError handleCpfException(CpfBadRequestException ex) {
+    return message(ex);
+  }
 
   private ApiError message(Exception ex) {
     var message = ex.getMessage();
