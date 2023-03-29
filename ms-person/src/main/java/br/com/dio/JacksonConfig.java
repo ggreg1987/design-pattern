b1d01b191ed0e47200f2d8dd0977640425b227b8
ggreg1987 <gabriel.gregorio1987@gmail.com>
2023-03-29 18:27:44 -0300

    Config and create a bean to ObjectMapper

--- ms-person/src/main/java/br/com/dio/JacksonConfig.java
+++ ms-person/src/main/java/br/com/dio/JacksonConfig.java
@@ -1,7 +1,28 @@
 package br.com.dio;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import static com.fasterxml.jackson.databind.DeserializationFeature.*;
 
 @Configuration
 public class JacksonConfig {
+
+
+  @Bean
+  public ObjectMapper objectMapper() {
+
+    var mapper = new ObjectMapper();
+    mapper.configure(ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+    mapper.configure(FAIL_ON_UNKNOWN_PROPERTIES, false);
+    mapper.configure(USE_JAVA_ARRAY_FOR_JSON_ARRAY, true);
+    mapper.configure(UNWRAP_SINGLE_VALUE_ARRAYS, true);
+    mapper.configure(ACCEPT_EMPTY_ARRAY_AS_NULL_OBJECT, false);
+    mapper.configure(FAIL_ON_IGNORED_PROPERTIES, true);
+    mapper.configure(FAIL_ON_MISSING_CREATOR_PROPERTIES, true);
+    mapper.configure(FAIL_ON_NULL_CREATOR_PROPERTIES, true);
+
+    return mapper;
+  }
 }
