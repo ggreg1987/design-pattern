eec2a7498881e4da8d93250cf0434c252e3f7eda
ggreg1987 <gabriel.gregorio1987@gmail.com>
2023-03-29 20:05:52 -0300

    Creating a String to get a exchange propertie

--- ms-person/src/main/java/br/com/dio/config/RabbitMQConfig.java
+++ ms-person/src/main/java/br/com/dio/config/RabbitMQConfig.java
@@ -1,4 +1,11 @@
 package br.com.dio.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class RabbitMQConfig {
+
+  @Value("${rabbitmq.person.exchange}")
+  private String exchange;
 }
