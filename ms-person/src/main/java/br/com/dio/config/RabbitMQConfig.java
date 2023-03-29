5280258b0aeaff45e4c8bb1164784a4b95c4b794
ggreg1987 <gabriel.gregorio1987@gmail.com>
2023-03-29 20:07:49 -0300

    Creating a messeger convert

--- ms-person/src/main/java/br/com/dio/config/RabbitMQConfig.java
+++ ms-person/src/main/java/br/com/dio/config/RabbitMQConfig.java
@@ -2,6 +2,8 @@ package br.com.dio.config;
 
 import org.springframework.amqp.core.Exchange;
 import org.springframework.amqp.core.ExchangeBuilder;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,5 +20,11 @@ public class RabbitMQConfig {
         .directExchange(exchange)
         .durable(true)
         .build();
+  }
+
+
+  @Bean
+  public MessageConverter convertMessage() {
+    return new Jackson2JsonMessageConverter();
   }
 }
