863d40c2b40417014592078cf49f8d614f303af7
ggreg1987 <gabriel.gregorio1987@gmail.com>
2023-03-29 19:17:20 -0300

    Endpoint create

--- ms-person/src/main/java/br/com/dio/domain/rest/resource/PersonResource.java
+++ ms-person/src/main/java/br/com/dio/domain/rest/resource/PersonResource.java
@@ -1,10 +1,14 @@
 package br.com.dio.domain.rest.resource;
 
+import br.com.dio.domain.entity.Person;
+import br.com.dio.domain.rest.dto.PersonDTO;
 import br.com.dio.domain.rest.service.PersonService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping("person")
@@ -13,5 +17,13 @@ public class PersonResource {
 
   private final PersonService service;
   private final ObjectMapper mapper;
+
+  @PostMapping(produces = APPLICATION_JSON_VALUE,
+      consumes = APPLICATION_JSON_VALUE)
+  @ResponseStatus(CREATED)
+  public PersonDTO create(@RequestBody Person person) throws Exception {
+    var entity = service.save(person);
+    return mapper.convertValue(entity, PersonDTO.class);
+  }
 
 }
