e3a11cdf1c694d72f4029583404cb65bcb5cdd04
ggreg1987 <gabriel.gregorio1987@gmail.com>
2023-03-29 18:30:53 -0300

    Creating a CpfBadException class

--- ms-person/src/main/java/br/com/dio/config/JacksonConfig.java
+++ ms-person/src/main/java/br/com/dio/config/JacksonConfig.java
@@ -1,4 +1,4 @@
-package br.com.dio;
+package br.com.dio.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
---
+++ ms-person/src/main/java/br/com/dio/exception/CpfBadException.java
@@ -0,0 +1,8 @@
+package br.com.dio.exception;
+
+public class CpfBadException extends RuntimeException {
+
+  public CpfBadException(String message) {
+    super(message);
+  }
+}
