5b31b4650dd39034e4b0ea202f12909142ec9820
ggreg1987 <gabriel.gregorio1987@gmail.com>
2023-03-29 20:06:55 -0300

    Creating a Exchange bean

--- ms-person/src/main/java/br/com/dio/config/RabbitMQConfig.java
+++ ms-person/src/main/java/br/com/dio/config/RabbitMQConfig.java
@@ -1,6 +1,9 @@
 package br.com.dio.config;
 
+import org.springframework.amqp.core.Exchange;
+import org.springframework.amqp.core.ExchangeBuilder;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -8,4 +11,12 @@ public class RabbitMQConfig {
 
   @Value("${rabbitmq.person.exchange}")
   private String exchange;
+
+  @Bean
+  public Exchange getExchange() {
+    return ExchangeBuilder
+        .directExchange(exchange)
+        .durable(true)
+        .build();
+  }
 }
