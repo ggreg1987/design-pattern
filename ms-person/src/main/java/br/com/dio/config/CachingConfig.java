72a3f72a681faa41e90d6891e3bb851ce7ebb55a
ggreg1987 <gabriel.gregorio1987@gmail.com>
2023-03-30 14:23:18 -0300

    Chancing cacheable to CacheManager bean

--- ms-person/src/main/java/br/com/dio/config/CachingConfig.java
+++ ms-person/src/main/java/br/com/dio/config/CachingConfig.java
@@ -4,10 +4,15 @@ import br.com.dio.domain.entity.Person;
 import br.com.dio.domain.repository.PersonRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 
@@ -22,9 +27,12 @@ public class CachingConfig {
     private final PersonRepository repository;
 
 
-    @Cacheable("person")
-    public Person getPersonByCpf(String cpf) {
-        log.info("Get Person by CPF {}",cpf);
-        return repository.findById(cpf).orElse(null);
+    @Bean
+    public CacheManager cacheManager() {
+        SimpleCacheManager cacheManager = new SimpleCacheManager();
+        cacheManager.setCaches(Arrays.asList(
+                new ConcurrentMapCache("person")
+        ));
+        return cacheManager;
     }
 }
