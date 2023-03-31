e46f9246508647e2134248f6b40dc55c2daf22a7
ggreg1987 <gabriel.gregorio1987@gmail.com>
2023-03-31 19:27:51 -0300

    FindAll endpoint

--- ms-address/src/main/java/br/com/dio/domain/rest/resource/AddressResource.java
+++ ms-address/src/main/java/br/com/dio/domain/rest/resource/AddressResource.java
@@ -7,6 +7,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.springframework.http.HttpStatus.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -46,5 +49,15 @@ public class AddressResource {
   @ResponseStatus(NO_CONTENT)
   public void delete(@PathVariable Long id) {
     service.delete(id);
+  }
+
+  @GetMapping(produces = APPLICATION_JSON_VALUE)
+  @ResponseStatus(OK)
+  public List<AddressDTO> findAll(@RequestBody Address address) {
+    var entityList = service.findAll(address);
+    return entityList
+        .stream()
+        .map(entity -> mapper.convertValue(entity,AddressDTO.class))
+        .collect(Collectors.toList());
   }
 }
