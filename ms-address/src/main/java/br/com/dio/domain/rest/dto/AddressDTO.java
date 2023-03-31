d11b69c950199558f14476b8c8252d83e2f090f8
ggreg1987 <gabriel.gregorio1987@gmail.com>
2023-03-31 15:22:52 -0300

    Creating AddressDTO

---
+++ ms-address/src/main/java/br/com/dio/domain/rest/dto/AddressDTO.java
@@ -0,0 +1,19 @@
+package br.com.dio.domain.rest.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+public class AddressDTO
+    extends RepresentationModel<AddressDTO>
+    implements Serializable {
+
+  private Long id;
+  private String street;
+  private String number;
+  private String zipcode;
+}
