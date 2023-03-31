1108cb560a463af351183b7db7b69a6ca298ceff
ggreg1987 <gabriel.gregorio1987@gmail.com>
2023-03-31 14:47:24 -0300

    Creating Address class

---
+++ ms-address/src/main/java/br/com/dio/domain/entity/Address.java
@@ -0,0 +1,49 @@
+package br.com.dio.domain.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "ADDRESS")
+public class Address implements Serializable {
+
+  @Id
+  @GeneratedValue(strategy = IDENTITY)
+  @Column(name = "ID")
+  private Long id;
+  @NotEmpty
+  @Column(name = "STREET", nullable = false, length = 30)
+  private String street;
+  @NotEmpty
+  @Column(name = "NUMBER", nullable = false, length = 10)
+  private String number;
+  @NotEmpty
+  @Column(name = "ZIPCODE", nullable = false, length = 8)
+  private String zipcode;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Address address = (Address) o;
+    return Objects.equals(getId(), address.getId());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(getId());
+  }
+}
