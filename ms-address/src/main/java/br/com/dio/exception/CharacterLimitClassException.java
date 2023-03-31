6084cc7805e9c2fd083e1676993b7575015fb861
ggreg1987 <gabriel.gregorio1987@gmail.com>
2023-03-31 19:36:23 -0300

    Creating CharacterLimitException class

---
+++ ms-address/src/main/java/br/com/dio/exception/CharacterLimitClassException.java
@@ -0,0 +1,8 @@
+package br.com.dio.exception;
+
+public class CharacterLimitClassException extends Exception {
+
+  public CharacterLimitClassException(String message) {
+    super(message);
+  }
+}
