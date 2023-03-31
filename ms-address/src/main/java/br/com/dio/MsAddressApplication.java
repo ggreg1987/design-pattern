d58684a6938b19887c6cbcfc0777be2d6413199b
ggreg1987 <gabriel.gregorio1987@gmail.com>
2023-03-31 19:45:31 -0300

    Adding OpenApi annotation

--- ms-address/src/main/java/br/com/dio/MsAddressApplication.java
+++ ms-address/src/main/java/br/com/dio/MsAddressApplication.java
@@ -1,9 +1,16 @@
 package br.com.dio;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Restful Address API", version = "1.0",
+		description = "Bootcamp by DIO",
+		contact = @Contact(name = "Gabriel Gregorio",
+		email = "gr3g1987@gmail.com",url = "http://github.com/ggreg1987")))
 public class MsAddressApplication {
 
 	public static void main(String[] args) {
