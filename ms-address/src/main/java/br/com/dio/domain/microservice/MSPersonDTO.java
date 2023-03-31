07f7baef2430697cdfd8cf3cee4b649abf139f06
ggreg1987 <gabriel.gregorio1987@gmail.com>
2023-03-31 20:08:28 -0300

    Fixing an error, changind somethings in MSPersonDTO

--- ms-address/src/main/java/br/com/dio/domain/microservice/MSPersonDTO.java
+++ ms-address/src/main/java/br/com/dio/domain/microservice/MSPersonDTO.java
@@ -1,12 +1,16 @@
 package br.com.dio.domain.microservice;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class MSPersonDTO {
 
+  @Id
   private String cpf;
   private String name;
 }
