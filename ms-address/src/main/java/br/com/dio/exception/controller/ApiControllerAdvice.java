88406e28b0b92ca9e6389d20a80e1f2595622ca3
ggreg1987 <gabriel.gregorio1987@gmail.com>
2023-03-31 15:45:14 -0300

    Creating a handler to AddressNotFoundException

--- ms-address/src/main/java/br/com/dio/exception/controller/ApiControllerAdvice.java
+++ ms-address/src/main/java/br/com/dio/exception/controller/ApiControllerAdvice.java
@@ -1,7 +1,20 @@
 package br.com.dio.exception.controller;
 
+import br.com.dio.exception.AddressNotFoundException;
+import br.com.dio.exception.ApiError;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestController
 public class ApiControllerAdvice {
+
+  @ExceptionHandler(AddressNotFoundException.class)
+  @ResponseStatus(NOT_FOUND)
+  public ApiError handleAddressNotFoundException(AddressNotFoundException ex) {
+    var msg = ex.getMessage();
+    return new ApiError(msg);
+  }
 }
