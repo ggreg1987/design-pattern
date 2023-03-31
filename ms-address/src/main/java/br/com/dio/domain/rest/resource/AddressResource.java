2c617e6661692f60418e16a5d09a544028309b60
ggreg1987 <gabriel.gregorio1987@gmail.com>
2023-03-31 19:20:05 -0300

    Create endpoint

--- ms-address/src/main/java/br/com/dio/domain/rest/resource/AddressResource.java
+++ ms-address/src/main/java/br/com/dio/domain/rest/resource/AddressResource.java
@@ -1,10 +1,14 @@
 package br.com.dio.domain.rest.resource;
 
+import br.com.dio.domain.entity.Address;
+import br.com.dio.domain.rest.dto.AddressDTO;
 import br.com.dio.domain.rest.service.AddressService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping("address")
@@ -13,4 +17,12 @@ public class AddressResource {
 
   private final AddressService service;
   private final ObjectMapper mapper;
+
+  @PostMapping(produces = APPLICATION_JSON_VALUE,
+      consumes = APPLICATION_JSON_VALUE)
+  @ResponseStatus(CREATED)
+  public AddressDTO create(@RequestBody Address address) {
+    var entity = service.save(address);
+    return mapper.convertValue(entity, AddressDTO.class);
+  }
 }
