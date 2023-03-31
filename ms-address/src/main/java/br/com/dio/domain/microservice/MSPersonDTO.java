07f7983f6d610ac59c4629eed86b6db889fe29aa
ggreg1987 <gabriel.gregorio1987@gmail.com>
2023-03-31 14:48:43 -0300

    Creating MSPersonDTO class

---
+++ ms-address/src/main/java/br/com/dio/domain/microservice/MSPersonDTO.java
@@ -0,0 +1,12 @@
+package br.com.dio.domain.microservice;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MSPersonDTO {
+
+  private String cpf;
+  private String name;
+}
