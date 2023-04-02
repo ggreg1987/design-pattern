412571738b60393cb3c6136620474a91cfdda4be
ggreg1987 <gabriel.gregorio1987@gmail.com>
2023-04-02 12:07:46 -0300

    Get Template

--- ms-address/src/main/java/br/com/dio/config/RabbitConfig.java
+++ ms-address/src/main/java/br/com/dio/config/RabbitConfig.java
@@ -2,6 +2,11 @@ package br.com.dio.config;
 
 import org.springframework.amqp.core.Exchange;
 import org.springframework.amqp.core.ExchangeBuilder;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,5 +23,18 @@ public class RabbitConfig {
         .directExchange(exchange)
         .durable(true)
         .build();
+  }
+
+  @Bean
+  @Autowired
+  public RabbitTemplate getTemplate(ConnectionFactory factory) {
+    final var rbTemplate = new RabbitTemplate(factory);
+    rbTemplate.setMessageConverter(convertMessage());
+    return rbTemplate;
+  }
+
+  @Bean
+  public MessageConverter convertMessage() {
+    return new Jackson2JsonMessageConverter();
   }
 }
