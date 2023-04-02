03b9632a1835a02f0260a6d8ce522c66994d88b9
ggreg1987 <gabriel.gregorio1987@gmail.com>
2023-04-02 12:13:24 -0300

    Adding MSPeronDTO

--- ms-address/src/main/java/br/com/dio/domain/microservice/MSPersonDTO.java
+++ ms-address/src/main/java/br/com/dio/domain/microservice/MSPersonDTO.java
@@ -1,8 +1,10 @@
 package br.com.dio.domain.microservice;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class MSPersonDTO {
 
   private String cpf;
--- ms-address/src/main/java/br/com/dio/domain/rest/dto/AddressDTO.java
+++ ms-address/src/main/java/br/com/dio/domain/rest/dto/AddressDTO.java
@@ -1,5 +1,6 @@
 package br.com.dio.domain.rest.dto;
 
+import br.com.dio.domain.microservice.MSPersonDTO;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.hateoas.RepresentationModel;
@@ -16,4 +17,5 @@ public class AddressDTO
   private String street;
   private String number;
   private String zipcode;
+  private MSPersonDTO personDTO;
 }
