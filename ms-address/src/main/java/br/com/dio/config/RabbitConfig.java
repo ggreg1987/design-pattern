d0cfa61e746e69fbf4ceb603b51904cbcbf194cb
ggreg1987 <gabriel.gregorio1987@gmail.com>
2023-04-02 12:07:14 -0300

    Get exchange

--- ms-address/src/main/java/br/com/dio/config/RabbitConfig.java
+++ ms-address/src/main/java/br/com/dio/config/RabbitConfig.java
@@ -1,7 +1,22 @@
 package br.com.dio.config;
 
+import org.springframework.amqp.core.Exchange;
+import org.springframework.amqp.core.ExchangeBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitConfig {
+
+  @Value("${rabbitmq.person.exchange}")
+  private String exchange;
+
+  @Bean
+  public Exchange getExchange() {
+    return ExchangeBuilder
+        .directExchange(exchange)
+        .durable(true)
+        .build();
+  }
 }
