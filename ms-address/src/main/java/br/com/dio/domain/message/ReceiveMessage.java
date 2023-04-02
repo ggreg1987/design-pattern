a91e6efe0163cb4b2f0f295a6200f96233ee2d79
ggreg1987 <gabriel.gregorio1987@gmail.com>
2023-04-02 12:22:12 -0300

    Creating method to receive message

--- ms-address/src/main/java/br/com/dio/domain/message/ReceiveMessage.java
+++ ms-address/src/main/java/br/com/dio/domain/message/ReceiveMessage.java
@@ -1,7 +1,25 @@
 package br.com.dio.domain.message;
 
+import br.com.dio.domain.entity.Person;
+import br.com.dio.domain.microservice.MSPersonDTO;
+import br.com.dio.domain.repository.PersonRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class ReceiveMessage {
+
+  private final PersonRepository repository;
+  private final ObjectMapper mapper;
+
+  @RabbitListener(queues = {"${rabbitmq.person.queue}"})
+  public void receiveMessage(@Payload MSPersonDTO dto) {
+    System.out.println(dto);
+    var entity = mapper.convertValue(dto,Person.class);
+    repository.save(entity);
+  }
 }
