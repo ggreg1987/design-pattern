2e627b0083e28ccad66755281e0696c226057767
ggreg1987 <gabriel.gregorio1987@gmail.com>
2023-04-02 15:01:34 -0300

    Creating a securityFilterChain bean

--- cloud-server/src/main/java/br/com/dio/config/SecurityConfig.java
+++ cloud-server/src/main/java/br/com/dio/config/SecurityConfig.java
@@ -1,9 +1,16 @@
 package br.com.dio.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
-  
+  @Bean
+  SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
+      throws Exception {
+
+  }
 }
